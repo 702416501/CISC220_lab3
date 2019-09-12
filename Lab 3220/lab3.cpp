16d41b9949f4d80794ea94810652e11e98a57681
Allie <adeabreu@udel.edu>
2019-09-12 16:11:05 -0400

    initial commit

---
+++ Lab 3220/lab3.cpp
@@ -0,0 +1,17 @@
+/*
+ * lab3.cpp
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: Allie DeAbreu, Alex Clark, Jason Belak
+ */
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+
+
+
